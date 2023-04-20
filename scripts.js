5670692aed806ae771c0f8aa654c0604883487db
bruno-araujo10 <brunoaraujo.developer@gmail.com>
2023-04-20 12:03:06 -0300

    updated controlls mobile

--- scripts.js
+++ scripts.js
@@ -7,40 +7,42 @@ const night = document.querySelector('.sol')
 
 const startGame = () => {
     audio.play();
-  }
-
+}
 
 const jump = () => {
     mario.classList.add('jump');
-
     audio.play();
-
-
     setTimeout(() => {
         mario.classList.remove('jump');
-        }, 500);
+    }, 500);
+}
+
+document.addEventListener('keydown', event => {
+    if (event.code === 'Space') {
+        jump();
     }
-    
-    const loop = setInterval(() => {
-        const pipePosition = pipe.offsetLeft;
-        const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
-    
-        if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
-            pipe.style.animation = 'none';
-            pipe.style.left = `${pipePosition}px`;
+});
 
-            mario.src = './assets/mario-dead.png'
-            mario.style.width = '75px'
-            mario.style.marginLeft = '50px'
-            gameOver.style.backgroundImage = 'url(./assets/game-over.webp)';
-            gameOver.style.backgroundRepeat = 'no-repeat';
-            gameOver.style.backgroundSize = 'cover';
-            gameOver.style.backgroundPosition = 'center center';
-            audio.pause()
-            night.src = './assets/lua.jpg'
-            deadSong.play();
-        }
-    }, 10);
+document.addEventListener('touchstart', event => {
+    jump();
+});
 
+const loop = setInterval(() => {
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
 
-document.addEventListener('keydown', jump);
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`;
+        mario.src = './assets/mario-dead.png'
+        mario.style.width = '75px'
+        mario.style.marginLeft = '50px'
+        gameOver.style.backgroundImage = 'url(./assets/game-over.webp)';
+        gameOver.style.backgroundRepeat = 'no-repeat';
+        gameOver.style.backgroundSize = 'cover';
+        gameOver.style.backgroundPosition = 'center center';
+        audio.pause()
+        night.src = './assets/lua.jpg'
+        deadSong.play();
+    }
+}, 10);
