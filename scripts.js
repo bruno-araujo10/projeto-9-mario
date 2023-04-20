12be7a5587e8e5a4ae62142101594bd31c780e2b
bruno-araujo10 <brunoaraujo.developer@gmail.com>
2023-04-20 10:44:25 -0300

    updated version 1.0

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
 const audio = document.getElementById("myAudio");
 const gameOver = document.querySelector('.game-board')
-const deadSong = new Audio('../assets/mario-dead-song.mp3');
+const deadSong = new Audio('./assets/mario-dead-song.mp3');
 const night = document.querySelector('.sol')
 
 const startGame = () => {
@@ -29,15 +29,15 @@ const jump = () => {
             pipe.style.animation = 'none';
             pipe.style.left = `${pipePosition}px`;
 
-            mario.src = '/assets/mario-dead.png'
+            mario.src = './assets/mario-dead.png'
             mario.style.width = '75px'
             mario.style.marginLeft = '50px'
-            gameOver.style.backgroundImage = 'url(../assets/game-over.webp)';
+            gameOver.style.backgroundImage = 'url(./assets/game-over.webp)';
             gameOver.style.backgroundRepeat = 'no-repeat';
             gameOver.style.backgroundSize = 'cover';
             gameOver.style.backgroundPosition = 'center center';
             audio.pause()
-            night.src = '/assets/lua.jpg'
+            night.src = './assets/lua.jpg'
             deadSong.play();
         }
     }, 10);
